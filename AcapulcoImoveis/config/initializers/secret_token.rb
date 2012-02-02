4ac825ab3b3d224947f435a0d0f5ef9ff5c6074c
Marcelo Domingues Silva <marcelo1982@gmail.com>
2012-02-02 16:13:16 -0200

    Aplicação rails zero

---
+++ AcapulcoImoveis/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcapulcoImoveis::Application.config.secret_token = 'fad09c4b4c24e3ffe9b4dff74d66da4f27dd93a5e1c48a2fea1cdf21a7b6e084fd8914d748920128791c2a909ec532b4babccaf77892736fae66aca4b33095e6'
